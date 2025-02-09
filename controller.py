a58ffff67b0e0b16d286117207321c320c006aa5
FuNK3Y <FuNK3Y@users.noreply.github.com>
2025-02-09 14:45:42 +0000

    Fix order

--- controller.py
+++ controller.py
@@ -11,20 +11,19 @@ class Controller:
         return "A" if Pin(Config.usb_gpio_pin, Pin.OUT).value() == 0 else "B"
 
     async def set_active_input(self, input=None):
-        async with self.__lock:
-
-            async def blink_led():
-                led = Pin(Config.led_gpio_pin, Pin.OUT)
-                try:
-                    while True:
-                        led.on()
-                        await asyncio.sleep(0.1)
-                        led.off()
-                        await asyncio.sleep(0.1)
-                except asyncio.CancelledError:
+        async def blink_led():
+            led = Pin(Config.led_gpio_pin, Pin.OUT)
+            try:
+                while True:
+                    led.on()
+                    await asyncio.sleep(0.1)
                     led.off()
-                    raise
+                    await asyncio.sleep(0.1)
+            except asyncio.CancelledError:
+                led.off()
+                raise
 
+        async with self.__lock:
             blink_led = asyncio.create_task(blink_led())
             if not input:
                 input = "A" if self.selected_input == "B" else "B"
