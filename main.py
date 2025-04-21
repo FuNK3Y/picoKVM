405f9900c1a752140521df3b8c5049e97c3b7f4c
FuNK3Y <FuNK3Y@users.noreply.github.com>
2025-04-21 18:39:40 +0000

    Stop catching unnecessary exceptions

--- main.py
+++ main.py
@@ -22,8 +22,4 @@ print("Connected, IP address: ", wlan.ifconfig()[0])
 
 asyncio.create_task(asyncio.start_server(WebServer(controller).handle_client, "0.0.0.0", 80))
 asyncio.create_task(ButtonHandler(Config.button_gpio_pin, controller.set_active_input).poll())
-
-try:
-    asyncio.get_event_loop().run_forever()
-except KeyboardInterrupt:
-    print("Program Interrupted by the user")
+asyncio.get_event_loop().run_forever()
