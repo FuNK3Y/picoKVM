fa06d1fe8bc4febb3a94d34d9d762c05cd6ad058
FuNK3Y <FuNK3Y@users.noreply.github.com>
2025-04-21 18:40:42 +0000

    Remove space

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ while not wlan.isconnected():
     print("Connecting to WLAN...")
     time.sleep(2)
 
-print("Connected, IP address: ", wlan.ifconfig()[0])
+print("Connected, IP address:", wlan.ifconfig()[0])
 
 asyncio.create_task(asyncio.start_server(WebServer(controller).handle_client, "0.0.0.0", 80))
 asyncio.create_task(ButtonHandler(Config.button_gpio_pin, controller.set_active_input).poll())
