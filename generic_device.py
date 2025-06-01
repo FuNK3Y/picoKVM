b5aa8036a9db4efd8c93f0e05cf689e5125a7603
FuNK3Y <FuNK3Y@users.noreply.github.com>
2025-06-01 18:13:56 +0000

    Reuse aiohttp over different call.

--- generic_device.py
+++ generic_device.py
@@ -3,13 +3,14 @@ from device import Device
 
 
 class GenericDevice(Device):
+
     def __init__(self, uri, method, kwargs={"A": {}, "B": {}}):
         super().__init__()
         self.uri = uri
         self.kwargs = kwargs
         self.method = method
+        self._clientSession = aiohttp.ClientSession()
 
     async def set_active_input(self, input):
-        async with aiohttp.ClientSession() as session:
-            async with session.request(self.method, self.uri, **self.kwargs[input]) as response:
-                return await response.text()
+        async with self._clientSession.request(self.method, self.uri, **self.kwargs[input]) as response:
+            return await response.text()
